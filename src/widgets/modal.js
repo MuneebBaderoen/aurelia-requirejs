1fc5bf5111fca66593aa74c89df7d6975069f4f8
Muneeb Baderoen <mbaderoen@gmail.com>
2015-09-22 23:30:09 +0200

    simpler condition for modal

--- src/widgets/modal.js
+++ src/widgets/modal.js
@@ -2,12 +2,4 @@ import {bindable} from 'aurelia-framework';
 
 export class Modal {
   @bindable router = null;
-
-  // isVisible = true;
-
-  get isVisible(){
-  	// console.log('hey')
-  	return this.router.isNavigating;
-  	// return true;
-  }
 }
